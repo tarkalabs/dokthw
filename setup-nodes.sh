25880df78fdf034f5c51b6367a1bf20dee03f57c
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2019-03-01 21:18:07 -0400

    uncomment code that was commented for testing

--- setup-nodes.sh
+++ setup-nodes.sh
@@ -2,11 +2,11 @@
 source ./utils.sh
 
 for instance in node-01 node-02; do
-  #run_remote ${instance} nodes/prepare-environment.sh
-  #smartscp nodes/99-loopback.conf ${instance}:/etc/cni/net.d/99-loopback.conf
-  #smartscp =(HOSTNAME=${instance} envsubst < nodes/kubelet-config.yaml) ${instance}:/var/lib/kubelet/kubelet-config.yaml
-  #smartscp nodes/kubelet.service ${instance}:/etc/systemd/system/kubelet.service
-  #smartscp nodes/kube-proxy-config.yaml ${instance}:/var/lib/kube-proxy/kube-proxy-config.yaml
-  #smartscp nodes/kube-proxy.service ${instance}:/etc/systemd/system/kube-proxy.service
+  run_remote ${instance} nodes/prepare-environment.sh
+  smartscp nodes/99-loopback.conf ${instance}:/etc/cni/net.d/99-loopback.conf
+  smartscp =(HOSTNAME=${instance} envsubst < nodes/kubelet-config.yaml) ${instance}:/var/lib/kubelet/kubelet-config.yaml
+  smartscp nodes/kubelet.service ${instance}:/etc/systemd/system/kubelet.service
+  smartscp nodes/kube-proxy-config.yaml ${instance}:/var/lib/kube-proxy/kube-proxy-config.yaml
+  smartscp nodes/kube-proxy.service ${instance}:/etc/systemd/system/kube-proxy.service
   run_remote ${instance} nodes/start-services.sh
 done
