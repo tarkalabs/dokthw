fa63780396dd28052b1104107629d87739e3dceb
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2019-03-01 21:20:04 -0400

    wait for a bit before setting up node authorization

--- control_plane/start-services.sh
+++ control_plane/start-services.sh
@@ -7,5 +7,8 @@ systemctl enable kube-apiserver kube-controller-manager kube-scheduler
 systemctl restart kube-apiserver kube-controller-manager kube-scheduler
 systemctl reload nginx
 
+echo "Waiting for cluster to come up..."
+sleep 10
+
 kubectl get componentstatuses --kubeconfig /var/lib/kubernetes/admin.kubeconfig
 kubectl apply -f node_authorization.yaml
